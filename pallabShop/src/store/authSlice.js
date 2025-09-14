b88f5d238e41e708374d4d3e525944bb93532836
amit chowdhury <camit8546@gmail.com>
2025-09-14 05:53:08 +0530

    eliminate duplicate product in redux store

--- pallabShop/src/store/authSlice.js
+++ pallabShop/src/store/authSlice.js
@@ -1,29 +1,31 @@
-import {createSlice} from '@reduxjs/toolkit';
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    status:true,
-    cart:[]
-}
+  status: true,
+  cart: [],
+};
 
-const authSlice=createSlice({
-    name:'auth',
-    initialState,
-    reducers:{
-        login:(state,action)=>{
-            state.status=true
-        },
-        logout:(state,action)=>{
-            state.status=false
-        },
-        addToCart:(state,action)=>{
-            state.cart.push(action.payload);
-        },
-        removeFromCart:(state,action)=>{
-            state.cart=state.cart.filter(item=>item.id !== action.payload);
-        },
+const authSlice = createSlice({
+  name: "auth",
+  initialState,
+  reducers: {
+    login: (state, action) => {
+      state.status = true;
+    },
+    logout: (state, action) => {
+      state.status = false;
+    },
+    addToCart: (state, action) => {
+      const itemExists = state.cart.find((item) => item.id === action.payload.id);
+      if (!itemExists) {
+        state.cart.push(action.payload);
+      } 
+    },
+    removeFromCart: (state, action) => {
+      state.cart = state.cart.filter((item) => item.id !== action.payload);
+    },
+  },
+});
 
-    }
-})
-
-export const {login,logout,addToCart,removeFromCart}=authSlice.actions;
+export const { login, logout, addToCart, removeFromCart } = authSlice.actions;
 export default authSlice.reducer;
