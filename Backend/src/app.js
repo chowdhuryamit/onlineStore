4b053137d45a36235149c7cd81d9f84171cd47b7
amit chowdhury <camit8546@gmail.com>
2025-10-04 22:13:41 +0530

    build production ready

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,12 +9,12 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.use(
-  cors({
-    origin: "*",
-    credentials: true,
-  })
-);
+// app.use(
+//   cors({
+//     origin: "*",
+//     credentials: true,
+//   })
+// );
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
