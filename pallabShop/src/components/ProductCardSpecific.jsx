20298d6d7d231c799866f43a31bec71c5f0df9e0
amit chowdhury <camit8546@gmail.com>
2025-10-12 22:21:11 +0530

    correct image alingment in the product grid card

--- pallabShop/src/components/ProductCardSpecific.jsx
+++ pallabShop/src/components/ProductCardSpecific.jsx
@@ -47,7 +47,7 @@ const ProductCardSpecific = ({ product,onDelete}) => {
         <img
           src={product.image}
           alt={product.name}
-          className="w-full h-full object-cover"
+          className="w-full h-full object-fill"
         />
         <div className="absolute inset-0 bg-gradient-to-t from-black/70 to-transparent"></div>
         <div className="absolute bottom-0 left-0 p-6">
